e242d06271b725cd1ef7947a073ef824bb562c76
zjl <1217105106@qq.com>
2020-05-11 11:29:41 +0800

    优化jsonrpc

--- jsonrpc/jsonrpc.go
+++ jsonrpc/jsonrpc.go
@@ -3,8 +3,10 @@ package jsonrpc
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 type JsonRpClient struct {
@@ -12,25 +14,48 @@ type JsonRpClient struct {
 	version string `json:"version"` //版本
 }
 
+//json rpc
+type Request struct {
+	Method string      `json:"method"` //方法名
+	Param  interface{} `json:"param"`  //参数
+}
+type Response struct {
+	Code int         `json:"code"` //状态码
+	Data interface{} `json:"data"` //数据
+	Msg  string      `json:"msg"`  //消息
+}
+
 func NewJsonRpClient(addr, version string) JsonRpClient {
 	return JsonRpClient{addr: addr, version: version}
 }
 
-func (j JsonRpClient) Call(param, result interface{}) error {
-	marshal, err := json.Marshal(param)
+func (j JsonRpClient) Call(method string, param, ret interface{}) error {
+	req := Request{
+		Method: method,
+		Param:  param,
+	}
+	marshal, err := json.Marshal(req)
 	if err != nil {
 		return err
 	}
-	resp, err := http.Post(j.addr, "application/json;charset=UTF-8", bytes.NewReader(marshal))
+	result, err := http.Post(j.addr, "application/json;charset=UTF-8", bytes.NewReader(marshal))
 	if err != nil {
 		return err
 	}
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(result.Body)
 	if err != nil {
 		return err
 	}
-	if err := json.Unmarshal(body, result); err != nil {
+	resp := Response{
+		Code: 0,
+		Data: ret,
+		Msg:  "",
+	}
+	if err := json.Unmarshal(body, &resp); err != nil {
 		return err
+	}
+	if resp.Code != 200 {
+		return errors.New("code:" + strconv.Itoa(resp.Code) + " " + resp.Msg)
 	}
 	return nil
 }
