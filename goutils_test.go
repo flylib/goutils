a127f9b67f99d42887bc74bfb15ebff1403460ab
zjl <zhangjunli@01lb.com.cn>
2021-04-22 15:28:51 +0800

    【add】logs

--- goutils_test.go
+++ goutils_test.go
@@ -2,11 +2,15 @@ package goutils
 
 import (
 	"fmt"
+	"github.com/zjllib/goutils/logs"
 	"github.com/zjllib/goutils/sort"
 	"testing"
 )
 
 func TestQuickSort(t *testing.T) {
+	logs.Info("error")
+	logs.Warn("error")
+	logs.Error("error")
 	arr := []int{1, 10, 7, 9}
 	sort.QuickSortByDESC(arr, 0, len(arr)-1)
 	fmt.Println(arr)
