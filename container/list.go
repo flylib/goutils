0f7df9653a515dd72b88e46853184282e0f94975
zanson <1217105106@qq.com>
2023-11-15 18:27:27 +0800

    up container/list

--- container/list.go
+++ container/list.go
@@ -59,7 +59,7 @@ func (l *List[T]) Init() *List[T] {
 }
 
 // New returns an initialized list.
-func New[T any]() *List[T] { return new(List[T]).Init() }
+func NewList[T any]() *List[T] { return new(List[T]).Init() }
 
 // Len returns the number of elements of list l.
 // The complexity is O(1).
